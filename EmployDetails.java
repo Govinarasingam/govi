06cb10a250576cf4172fb19208ba23cbde6a066f
Govinarasingam <tnarasingam7596@gmail.com>
2022-10-15 09:05:19 +0530

    framework

---
+++ EmployDetails.java
@@ -0,0 +1,34 @@
+package org.sample;
+
+public class EmployDetails {
+	// method creation
+	
+	private void employName () {
+        // logic
+		System.out.println("Employ name --- Narsingam");
+
+
+	}
+	private void employId() {
+		System.out.println("Employ Id = 123");
+	
+	}
+    // normally Execution start from the main met hod
+	
+	public static void main(String[] args) {
+		
+		// object creation 
+	    // classname objectreferencename = new Classname();
+		EmployDetails a = new EmployDetails();
+		
+		// to call a method
+		// objectrefername.methodname();
+		
+		a.employId();
+		a.employName();
+	
+
+		
+
+	}
+}
